cbb617ab00e338669ef45b3225cb0c41ab6c5000
maguah <maguahofficial@gmail.com>
2024-08-21 01:22:42 +0300

    0x05-Node_JS_basic

--- 0x05-Node_JS_basic/6-http_express.js
+++ 0x05-Node_JS_basic/6-http_express.js
@@ -9,14 +9,15 @@ const port = 1245; // Port 1245
 
 // Define a route for the root URL ('/') that sends a response to the client
 app.get('/', (request, response) => {
-  // Send a plain text response with the message 'Hello Holberton School!'
+// Send a plain text response with the message 'Hello Holberton School!'
   response.send('Hello Holberton School!');
 });
 
 // Make the Express application listen on the specified port
 app.listen(port, () => {
-  // This callback function can be used to log a message or perform an action when the server starts
-  // Currently, it does nothing
+// This callback function can be used to log a message or perform an
+// action when the server starts
+// Currently, it does nothing
 });
 
 // Export the Express application so it can be used in other files/modules
