3c70b7214102d11f5e5f3aa2b4a95d79b3ed2304
maguah <maguahofficial@gmail.com>
2024-08-21 01:50:49 +0300

    0x05-Node_JS_basic

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,6 +1,5 @@
-// Write a message to the console asking for the user's name
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
-
+// Write a message to the console asking for the user's name
   process.stdin.on('data', (data) => {
     process.stdout.write(`Your name is: ${data.toString()}`);
     process.exit();
